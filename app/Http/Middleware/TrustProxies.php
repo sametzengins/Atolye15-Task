38a10549fcc09317ad2ff4f40e8e099d3fe30ee8
Samet Zengin <hey@sametzengin.com>
2022-01-17 13:00:32 +0300

    Update

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Middleware;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware;
 class TrustProxies extends Middleware
 {
     protected $proxies = '*';
